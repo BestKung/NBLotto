d81094fd8d0891451bdb9fec0146d79b6f78a9eb
BestKung <best.kittiporn@gmail.com>
2020-11-18 22:06:45 +0700

    first commit

---
+++ src/main/java/com/best/nblotto/NbLottoApplication.java
@@ -0,0 +1,13 @@
+package com.best.nblotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbLottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbLottoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/best/nblotto/NbLottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.best.nblotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbLottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
