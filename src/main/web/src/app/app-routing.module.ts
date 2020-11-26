ecbb45b100ff4ab0485b23f04f41c7609d6d1139
BestKung <best.kittiporn@gmail.com>
2020-11-26 22:16:16 +0700

    add layout

--- src/main/web/src/app/app-routing.module.ts
+++ src/main/web/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LayoutComponent} from "./unsecure/layout/layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: LayoutComponent,
+  // children: [
+  //   {path: "dashboard", component: DashboardComponent, pathMatch: "full"},
+  //
+  // ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/web/src/app/app.module.ts
+++ src/main/web/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './unsecure/layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
