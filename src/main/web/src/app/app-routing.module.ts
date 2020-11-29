8748e7ea092ab1bca98d9850bd09c96f64514ea0
BestKung <best.kittiporn@gmail.com>
2020-11-29 22:06:50 +0700

    edit build ng

--- src/main/web/src/app/app-routing.module.ts
+++ src/main/web/src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LayoutComponent} from "./unsecure/layout/layout.component";
-import {DashboardComponent} from "./unsecure/dashboard/dashboard.component";
 import {LottoHisComponent} from "./unsecure/lotto-his/lotto-his.component";
+import {WebboardComponent} from "./unsecure/webboard/webboard.component";
+import {AnalyzeComponent} from "./unsecure/analyze/analyze.component";
+import {LoginComponent} from "./unsecure/login/login.component";
 
 const routes: Routes = [{
   path: '',
   component: LayoutComponent,
   children: [
-    {path: "dashboard", component: DashboardComponent, pathMatch: "full"},
     {path: "lotto-his", component: LottoHisComponent, pathMatch: "full"},
+    {path: "analyze", component: AnalyzeComponent, pathMatch: "full"},
+    {path: "webboard", component: WebboardComponent, pathMatch: "full"},
+    {path: "login", component: LoginComponent, pathMatch: "full"},
   ]
 }];
 
--- src/main/web/src/app/app.module.ts
+++ src/main/web/src/app/app.module.ts
@@ -13,13 +13,19 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatMenuModule} from "@angular/material/menu";
 import { LottoHisComponent } from './unsecure/lotto-his/lotto-his.component';
 import { DashboardComponent } from './unsecure/dashboard/dashboard.component';
+import { AnalyzeComponent } from './unsecure/analyze/analyze.component';
+import { WebboardComponent } from './unsecure/webboard/webboard.component';
+import { LoginComponent } from './unsecure/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     LottoHisComponent,
-    DashboardComponent
+    DashboardComponent,
+    AnalyzeComponent,
+    WebboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
